685d2abfe33f49fd17b5e573977197ec594d0e49
notGonim <mhmodgonim96@gmail.com>
2021-04-19 05:42:23 +0200

    CREATE server and run on port 5000

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,24 @@ import dotEnv from 'dotenv'
  
 
 
+//add the port that server will run on which is going to be 5000
+const port = process.env.PORT || 5000
+
+
+
+
+//setting up config file 
+dotEnv.config({ path: './config/config.env' })
+
+
+// connecting to the database
+ 
+
+
+
+
+//Create server && run it on port 5000
+const server = app.listen(port, () => {
+    console.log(`server is running on : ${port} in development `)
+})
+
