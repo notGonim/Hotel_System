98d0d72c391be109ededc1cabec2f63c9d810e8a
notGonim <mhmodgonim96@gmail.com>
2021-04-21 17:34:10 +0200

    ADDED the login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from "react-router-dom";
+
+
 
 export const Login = () => {
+
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const dispatch = useDispatch();
+    const history = useHistory()
+
+    const onHandleLogin = (e) => {
+        e.preventDefault()
+
+    }
+
     return (
-        <div className="container-fluid h1 p-5 text-center">
-            Login Page
-        </div>
+
+        <>
+            <div className="container-fluid bg-secondary p-5 text-center">
+                <h1> Register</h1>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <div className="form-group mb-3">
+                            <label >Email : </label>
+                            <input type="email" value={email} className="form-control" placeholder="Write Your Email" onChange={(e) => setEmail(e.target.value)} />
+                            <label >Password : </label>
+                            <input type="password" value={password} className="form-control" placeholder="Write Your Password " onChange={(e) => setPassword(e.target.value)} />
+                            { /*error &&
+                                <div class="alert alert-danger m-1"  >{error}</div>
+  */                      }
+
+                            <button className="btn btn-outline-success mt-3" onClick={onHandleLogin}>Login</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+
     )
 }
