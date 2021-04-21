983ddf9c7b2f91fb06620040a2a4dd84b2c9279f
notGonim <mhmodgonim96@gmail.com>
2021-04-21 15:51:47 +0200

    Encrpting password before saving in databases

--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -32,5 +32,15 @@ const userSchema = new mongoose.Schema({
 
 }, { timestamps: true })
 
+//Encrypting user password before saving 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password'))
+        next()
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+
+
+
 const User = mongoose.model('Users', userSchema)
 export default User
