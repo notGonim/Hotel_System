65b30c460092d3c6280b74f497a0778284642d02
notGonim <mhmodgonim96@gmail.com>
2021-04-22 20:26:19 +0200

    ADDED jwtTokens func where i send token via cookie

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -18,3 +18,30 @@ export const userRegister = asyncError(async (req, res, next) => {
     })
 
 })
+
+
+//handle user login       api/login
+export const login = asyncError(async (req, res, next) => {
+
+    //get user password and email
+    const { email, password } = req.body
+
+    //check if password && email entered by user
+    if (!password || !email)
+        return next(asyncError('Please enter your email and password ', 401))
+
+    //search user`s email in database and get it if its exists
+    const user = await User.findOne({ email }).select('+password')
+
+    //check if user is in our databases
+    if (!user)
+        return next(asyncError('User not found please enter valid email', 401))
+
+    //check if the user`s password is correct
+    const isPasswordMatch = await user.matchPassword(password)
+    if (!isPasswordMatch)
+        return next(asyncError('User not found Please enter Valid Email and Password', 401))
+
+    //sending token    
+
+})
