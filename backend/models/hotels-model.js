7dec2d5c02ba2139a4c7a97e53dd5db822dd9cad
notGonim <mhmodgonim96@gmail.com>
2021-04-24 20:24:48 +0200

    ADDED the hotel model

---
+++ backend/models/hotels-model.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+
+const hotelSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please enter Hotel name'],
+            maxLength: [30, 'Hotel name cannot exceed 30 characters'],
+            unique: [true, 'Hotels name exists in our databases please enter a new username']
+        },
+        email: {
+            type: String,
+            required: [true, 'Please enter Hotel email'],
+            unique: true,
+            validate: [validator.isEmail, 'Please enter valid email address']
+        },
+        rooms: {
+            type: number,
+            required: [true, 'Please enter Hotel number of rooms '],
+        }
+
+    }
+)
+
+
+
+
+const Hotels = mongoose.model('Hotels', hotelSchema)
+export default Hotels
