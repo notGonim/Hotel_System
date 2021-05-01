126d995066b862bfecbc603f9f130d8b7368296b
notGonim <mhmodgonim96@gmail.com>
2021-05-01 05:28:03 +0200

    Added Confirmed component

--- client/src/components/booking/Home.js
+++ client/src/components/booking/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { AddHotel } from '../admin/AddHotel'
 import { HotelCard } from '../hotels/HotelCard'
+import { Confirmed } from '../layout/checked'
 
 export const Home = () => {
     return (
         <div className="container-fluid  p-1">
-            <AddHotel />
+
+            <Confirmed />
         </div>
     )
 }
--- client/src/components/layout/checked.js
+++ client/src/components/layout/checked.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export const checked = () => {
+export const Confirmed = () => {
     return (
-        <div>
-            
+        <div className="container text-center p-5" >
+            <h2 className="p-5">Your Order has been placed successfully.</h2>
+            <img src={process.env.PUBLIC_URL + '/success.png'} className="rounded mx-auto d-block" style={{ width: "20rem" }} />
+
         </div>
     )
 }
