f56f3220e6da2f71784368f0adcd3dc011c186eb
notGonim <mhmodgonim96@gmail.com>
2021-05-04 09:12:18 +0200

    ADDED loader to Frontend

--- client/src/components/hotels/Hotel.js
+++ client/src/components/hotels/Hotel.js
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Footer } from '../layout/Footer'
 import { HotelCard } from './HotelCard'
 import { MainHeader } from '../layout/header'
+import { useDispatch, useSelector } from 'react-redux'
+import { Loader } from '../layout/Loader'
+import { getHotels } from '../../redux/hotels/hotels-actions'
 
 export const Hotel = () => {
+
+
+    const { hotels, error, loading } = useSelector(state => state.hotels);
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getHotels())
+    }, [dispatch, error])
+
     return (
         <>
             <MainHeader />
-            <div className="container p-3">
 
-                <div className="row ">
-                    <div className="col-sm mb-3">
-                        <HotelCard />
+            {loading ?
+                <Loader />
+                :
+                <div className="container p-3">
+
+                    <div className="row ">
+                        <div className="col-sm mb-3">
+                            <HotelCard />
+                        </div>
                     </div>
-
-
                 </div>
-            </div>
+            }
             <br />
         </>
     )
--- client/src/components/layout/Loader.js
+++ client/src/components/layout/Loader.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export const Loader = () => {
     return (
-        <div class="spinner-border text-info" role="status">
+        <div className="d-flex justify-content-center  align-items-center" style={{ height: "28rem" }}>
+            <div class="spinner-border text-info" role="status">
+            </div>
         </div>
     )
 }
