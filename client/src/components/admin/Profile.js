7b38f760f8db81ecd64e4371e227382c0eb74356
notGonim <mhmodgonim96@gmail.com>
2021-05-04 07:05:26 +0200

    ADDED loader component

--- client/src/components/admin/Profile.js
+++ client/src/components/admin/Profile.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { MainHeader } from '../layout/header'
+import { Loader } from '../layout/Loader'
 
 export const Profile = () => {
     return (
         <>
             <MainHeader />
-            dasdasdas
+            <Loader />
         </>
     )
 }
---
+++ client/src/components/layout/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Loader = () => {
+    return (
+        <div class="spinner-border text-info" role="status">
+        </div>
+    )
+}
