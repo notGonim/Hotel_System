887796bc5fea5dbf0751e5f29406d0b63755beb3
notGonim <mhmodgonim96@gmail.com>
2021-05-04 06:11:41 +0200

    Redesign the header

--- client/src/components/layout/Nav.js
+++ client/src/components/layout/Nav.js
@@ -3,11 +3,27 @@ import { Link } from 'react-router-dom'
 
 export const Nav = () => {
     return (
-        <div className="nav bg-dark d-flex justify-content-between ">
-            <Link to='/' className='nav-link'>Home</Link>
-            <Link to='/login' className='nav-link'> LogIn</Link>
-            <Link to='/register' className='nav-link'> Register</Link>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link to="/" className="navbar-brand" >HotelMe</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon">
+                <div className="">
+                </div>
+                </span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <Link to="/" className="nav-link" >Home <span className="sr-only">(current)</span></Link>
+                    </li>
+                </ul>
+                <div className="">
+                    <Link to="/login" className="btn btn-outline-primary mr-1">Sign In</Link>
+                    <Link to="/register" className="btn btn-outline-success">Register</Link>
+                </div>
+            </div>
+        </nav>
     )
 }
 
