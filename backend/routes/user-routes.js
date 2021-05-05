6122127716870ae04572e46fd27e2b294c1557b9
notGonim <mhmodgonim96@gmail.com>
2021-05-05 06:17:03 +0200

    ADDED Edit profile func

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { userRegister, login, logout } from '../controllers/user-controller.js'
+import { userRegister, login, logout, editUserData } from '../controllers/user-controller.js'
 
 export const UserRouter = express.Router()
 
@@ -9,3 +9,4 @@ export const UserRouter = express.Router()
 UserRouter.post('/register', userRegister)
 UserRouter.post('/login', login)
 UserRouter.get('/logout', logout)
+UserRouter.put('/edit', editUserData)
